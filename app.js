7e9f9cb43b7d81b6160733c618695adb611e0af9
atomdellow <36104813+atomdellow@users.noreply.github.com>
2022-05-28 23:46:14 -0400

    another heroku deploy

--- app.js
+++ app.js
@@ -99,7 +99,7 @@ passport.deserializeUser(function(id, done) {
 
 passport.use(new GoogleStrategy({
 
-    clientID: process.env.clientID,
+    clientID: process.env.CLIENTID,
     clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "http://localhost:3000/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
---
+++ deploy.sh
@@ -0,0 +1,38 @@
+echo "current dir: " $PWD
+env -i
+#echo "starting git add"
+#if [[ "$PWD" = /c/Users/owner/Documents/zen/zen ]]
+#then
+#	echo "Delete Zen"
+#	cd ..
+#	echo $PWD
+#	echo "Deleting Directory"
+#if[[ "$PWD" = /c/Users/owner/Documents/zen ]]
+#then
+#	&& echo "Running Git"
+#	echo "starting git add"
+#	git add .
+#	wait -n
+#	git commit -m
+#	wait -n
+#fi
+
+#git add .
+#wait -n
+#git commit -m
+#wait -n
+#git push -u origin main
+#wait -n
+#git clone https://github.com/atomdellow/zen.git
+#wait -n
+#cd zen
+#wait -n
+#heroku create
+#wait -n
+#git push heroku main
+#wait -n
+#heroku ps:scale web=1
+#wait -n
+#heroku open 
+#wait -n
+#cd .. 
